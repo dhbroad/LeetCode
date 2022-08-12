31df51f1facb60774d347629b0d9cbb2317db7b4
dhbroad <dhbroad@udel.edu>
2022-08-11 21:38:16 -0400

    1 solution. Faster than 97.4% of submissions for this problem on LeetCode

---
+++ 1 Two Sum.py
@@ -0,0 +1,19 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numsDict = {}
+        i = 0
+        for num in nums:
+            if target-num in numsDict:
+                return [numsDict[target-num], i]
+            else:
+                numsDict[num] = i
+                i += 1
+
+n1 = [1,3,5,9]
+t1 = 10
+
+print(twoSum(n1, t1))
