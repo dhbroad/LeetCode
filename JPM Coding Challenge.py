258dbf16b3aa77bbdd44af4139fd64dc74d54d37
dhbroad <dhbroad@udel.edu>
2022-08-15 21:37:36 -0400

    JPM Coding problems

---
+++ JPM Coding Challenge.py
@@ -0,0 +1,23 @@
+# from doctest import Example
+# import numbers
+
+
+# closest numbers
+# for a list of numbers, return all of the pairs of numbers that have the lowest difference between them.
+
+# Given an array of distinct integers, determine the minimum absolute difference between any two elements.
+# Print all element pairs with that difference in ascending order.ord
+
+# Example
+# numbers = [6,2,4,10]
+# the minimum absolute difference is 2 and the pairs with that difference are (2,4) and (4,6). When printing element pairs (i,j), they should be ordered ascending first by i and then by j.bytearray
+# 2 4
+# 4 6
+
+# Function description
+# Complete the function closestNumbers in the editor.
+
+
+
+# break a palindrome
+# has to be lower and no longer be a palindrome
