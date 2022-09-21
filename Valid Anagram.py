10da85fb2389c7575fc19eece15474d0e9231e3f
dhbroad <dhbroad@udel.edu>
2022-09-20 22:08:43 -0400

    valid anagram

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -24,3 +24,41 @@
  
 
 # Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+
+def isAnagram(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    s_dict = {}
+    t_dict = {}
+    for a in s:
+        if a in s_dict:
+            s_dict[a] += 1
+        else:
+            s_dict[a] = 1
+    for b in t:
+        if b in t_dict:
+            t_dict[b] += 1
+        else:
+            t_dict[b] = 1 
+    if s_dict == t_dict:
+        return True
+    else:
+        return False
+
+
+s1 = 'anagram'
+t1 = 'nagaram'
+
+s2 = 'ab'
+t2 = 'ba'
+
+s3 = 'fail'
+t3 = 'pass'
+
+print(isAnagram(s1, t1))
+print(isAnagram(s2, t2))
+print(isAnagram(s3, t3))
