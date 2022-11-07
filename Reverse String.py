79ed178bbc0463b148b5ded3d27f58c408624634
dhbroad <dhbroad@udel.edu>
2022-11-06 21:22:45 -0500

    reverse string solution

--- Reverse String.py
+++ Reverse String.py
@@ -18,3 +18,25 @@
 
 # 1 <= s.length <= 105
 # s[i] is a printable ascii character.
+
+def reverseString(s):
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+    """
+    l = len(s)
+    j = l-1
+    i = 0
+    k = ""
+    while i < l/2:
+        k = s[i]
+        s[i] = s[j]
+        s[j] = k
+        i+=1
+        j-=1
+    return s
+
+
+string1 = ["h", "e", "l", "l", "o"]
+
+print(reverseString(string1))
