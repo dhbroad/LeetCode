cd0185b37b8b39c9536d84bf1673bac7417f4db6
dhbroad <dhbroad@udel.edu>
2022-12-14 20:53:33 -0500

    solution

---
+++ Solved/Medium/189 Rotate Array.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/rotate-array/
+
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -231 <= nums[i] <= 231 - 1
+# 0 <= k <= 105
+
+def rotate(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    nums_length = len(nums)
+    if nums_length == 1:
+        return
+    new_index = 0
+    rotated_array = []
+    for i in range(nums_length):
+        rotated_array.append(0)
+    for i in range(nums_length):
+        rotated_array[(i+k)%nums_length] = nums[i]
+    for i in range(nums_length):
+        nums[i] = rotated_array[i]
+        
